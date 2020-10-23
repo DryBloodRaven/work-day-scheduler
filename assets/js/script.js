edadb86d7da97385a26afdabe7cb1adec6609561
DryBloodRaven <amproctor@gmail.com>
2020-10-23 00:14:29 -0700

    added starter code

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Make HTML have columns and rows correspond to certain times
+
+// Only 1 Function needs to register what time it is
+
+// Loop over all boxes and conditionally decide what color they need to boxes
+
+// Set up localStorage
